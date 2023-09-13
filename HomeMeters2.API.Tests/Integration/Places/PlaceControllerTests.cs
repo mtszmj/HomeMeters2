cfb065bd5b3fae887854e0d419489cf9a0133f80
Mateusz <mateuszmaj@o2.pl>
2023-09-13 20:35:04 +0200

    test: Add tests to PlaceController

--- HomeMeters2.API.Tests/Integration/Places/PlaceControllerTests.cs
+++ HomeMeters2.API.Tests/Integration/Places/PlaceControllerTests.cs
@@ -1,32 +1,112 @@
 using System.Net.Http.Json;
 using System.Text.Json;
-using HomeMeters2.API.Places;
 using HomeMeters2.API.Places.Dtos;
 
 namespace HomeMeters2.API.Tests.Integration.Places;
 
 public class PlaceControllerTests : IntegrationTestsBase
 {
-    [Test]
-    public async Task place_endpoint_get_returns_ok()
-    {
-        var response = await Client.GetAsync("/api/place");
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-    }
+    private const string EndpointUri = "/api/place";
+    private const string DeletedEndpointUri = "/api/place/deleted";
     
     [Test]
-    public async Task place_endpoint_create_returns_created()
+    public async Task get_returns_ok()
+    {
+        var response = await Client.GetAsync(EndpointUri);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Test]
+    public async Task get_returns_two_places()
+    {
+        // arrange
+        _ = await PostCreatePlace("Test1", $"{nameof(get_returns_two_places)}_1");
+        _ = await PostCreatePlace("Test2", $"{nameof(get_returns_two_places)}_2");
+
+        // act
+        var response = await Client.GetAsync(EndpointUri);
+        
+        // assert 
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        var placesContent = await response.Content.ReadAsStringAsync();
+        var placesDto = JsonSerializer.Deserialize<IEnumerable<PlaceDto>>(placesContent, JsonSerializerOptions);
+        placesDto.Should().NotBeNull();
+        placesDto.Should().HaveCount(2);
+    }
+
+    [Test]
+    public async Task get_returns_places_not_deleted()
+    {
+        // arrange
+        var p1 = await PostCreatePlace("Test1", $"{nameof(get_returns_two_places)}_1");
+        var p2 = await PostCreatePlace("Test2", $"{nameof(get_returns_two_places)}_2");
+        var p3 = await PostCreatePlace("Test3", $"{nameof(get_returns_two_places)}_3");
+        var p4 = await PostCreatePlace("Test4", $"{nameof(get_returns_two_places)}_4");
+
+        await Client.DeleteAsync($"{EndpointUri}/{p2.Id}");
+        await Client.DeleteAsync($"{EndpointUri}/{p4.Id}");
+
+        // act
+        var response = await Client.GetAsync(EndpointUri);
+        
+        // assert 
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        var placesContent = await response.Content.ReadAsStringAsync();
+        var placesDto = JsonSerializer.Deserialize<PlaceDto[]>(placesContent, JsonSerializerOptions);
+        placesDto.Should().NotBeNull();
+        placesDto.Should().HaveCount(2);
+        placesDto.Select(x => x.Id).Should().BeEquivalentTo(new[] { 1, 3 });
+    }
+
+    [Test]
+    public async Task get_id_returns_single_place()
+    {
+        // arrange
+        var id = (await PostCreatePlace("Test1", $"{nameof(get_id_returns_single_place)}_1")).Id;
+        _ = await PostCreatePlace("Test2", $"{nameof(get_id_returns_single_place)}_2");
+        
+        // act
+        var response = await Client.GetAsync($"{EndpointUri}/{id}");
+        
+        // assert 
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        var placesContent = await response.Content.ReadAsStringAsync();
+        var placesDto = JsonSerializer.Deserialize<PlaceDto>(placesContent, JsonSerializerOptions);
+        placesDto.Should().NotBeNull();
+        placesDto.Id.Should().Be(id);
+        placesDto.Description.Should().Be($"{nameof(get_id_returns_single_place)}_1");
+    }
+
+    private async Task<PlaceDto> PostCreatePlace(string name, string description)
+    {
+        var dto = new CreatePlaceDto
+        {
+            Name = name,
+            Description = description
+        };
+        var content = JsonContent.Create(dto);
+        var createResponse = await Client.PostAsync(EndpointUri, content);
+        var createResponseContent = await createResponse.Content.ReadAsStringAsync();
+        var placeDto = JsonSerializer.Deserialize<PlaceDto>(createResponseContent, JsonSerializerOptions);
+        return placeDto ?? new PlaceDto { Id = -1, Name = "", Description = "" };
+    }
+
+    [Test]
+    public async Task create_returns_created()
     {
         // arrange
         var dto = new CreatePlaceDto
         {
             Name = "Test",
-            Description = "TestDescription"
+            Description = nameof(create_returns_created)
         };
         var content = JsonContent.Create(dto);
         
         // act
-        var response = await Client.PostAsync("/api/place", content);
+        var response = await Client.PostAsync(EndpointUri, content);
         
         // assert 
         response.StatusCode.Should().Be(HttpStatusCode.Created);
@@ -36,6 +116,121 @@ public class PlaceControllerTests : IntegrationTestsBase
         var placeDto = JsonSerializer.Deserialize<PlaceDto>(createdContent, JsonSerializerOptions);
         placeDto.Should().NotBeNull();
         placeDto.Name.Should().Be("Test");
-        placeDto.Description.Should().Be("TestDescription");
+        placeDto.Description.Should().Be(nameof(create_returns_created));
+    }
+
+    [Test]
+    public async Task delete_returns_no_content()
+    {
+        // arrange
+        var id1 = (await PostCreatePlace("Test1", $"{nameof(delete_returns_no_content)}_1")).Id;
+        var id2 = (await PostCreatePlace("Test2", $"{nameof(delete_returns_no_content)}_2")).Id;
+        var id3 = (await PostCreatePlace("Test3", $"{nameof(delete_returns_no_content)}_3")).Id;
+        
+        // act
+        var response = await Client.DeleteAsync($"{EndpointUri}/{id2}");
+        
+        // assert 
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Test]
+    public async Task get_deleted_id_returns_single_deleted_place()
+    {
+        // arrange
+        var id1 = (await PostCreatePlace("Test1", $"{nameof(get_deleted_id_returns_single_deleted_place)}_1")).Id;
+        var id2 = (await PostCreatePlace("Test2", $"{nameof(get_deleted_id_returns_single_deleted_place)}_2")).Id;
+        var id3 = (await PostCreatePlace("Test3", $"{nameof(get_deleted_id_returns_single_deleted_place)}_3")).Id;
+        
+        _ = await Client.DeleteAsync($"{EndpointUri}/{id2}");
+        _ = await Client.DeleteAsync($"{EndpointUri}/{id3}");
+
+        // act
+        var response = await Client.GetAsync($"{DeletedEndpointUri}/{id2}");
+
+        // assert 
+        var content = await response.Content.ReadAsStringAsync();
+        var placeDto = JsonSerializer.Deserialize<PlaceDeletedDto>(content, JsonSerializerOptions);
+        placeDto.Should().NotBeNull();
+        placeDto.Name.Should().Be("Test2");
+        placeDto.Description.Should().Be($"{nameof(get_deleted_id_returns_single_deleted_place)}_2");
+    }
+
+    [Test]
+    public async Task get_deleted_returns_only_deleted()
+    {
+        // arrange
+        var id1 = (await PostCreatePlace("Test1", $"{nameof(get_deleted_id_returns_single_deleted_place)}_1")).Id;
+        var id2 = (await PostCreatePlace("Test2", $"{nameof(get_deleted_id_returns_single_deleted_place)}_2")).Id;
+        var id3 = (await PostCreatePlace("Test3", $"{nameof(get_deleted_id_returns_single_deleted_place)}_3")).Id;
+        
+        _ = await Client.DeleteAsync($"{EndpointUri}/{id2}");
+        _ = await Client.DeleteAsync($"{EndpointUri}/{id3}");
+
+        // act
+        var response = await Client.GetAsync($"{DeletedEndpointUri}");
+
+        // assert 
+        var content = await response.Content.ReadAsStringAsync();
+        var places = JsonSerializer.Deserialize<PlaceDeletedDto[]>(content, JsonSerializerOptions);
+        places.Should().NotBeNull();
+        places.Should().HaveCount(2);
+        places[0].Should().NotBeNull();
+        places[0].Name.Should().BeOneOf("Test2", "Test3");
+        places[1].Should().NotBeNull();
+        places[1].Name.Should().BeOneOf("Test2", "Test3");
+        places[0].Name.Should().NotBe(places[1].Name);
+    }
+
+    [Test]
+    public async Task update_returns_no_content()
+    {
+        // arrange
+        var created = (await PostCreatePlace("Test1", $"{nameof(update_returns_no_content)}_1"));
+        
+
+        // act
+        var dto = new UpdatePlaceDto(created.Id, "Test1_updated", $"{nameof(update_returns_no_content)}_1_updated");
+        var updateContent = JsonContent.Create(dto);
+        var updateResponse = await Client.PutAsync(EndpointUri, updateContent);
+
+        // assert 
+        updateResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+    
+    [Test]
+    public async Task update_changes_place_data()
+    {
+        // arrange
+        var created = (await PostCreatePlace("Test1", $"{nameof(update_changes_place_data)}_1"));
+
+        // act
+        var dto = new UpdatePlaceDto(created.Id, "Test1_updated", $"{nameof(update_changes_place_data)}_1_updated");
+        var updateContent = JsonContent.Create(dto);
+        _ = await Client.PutAsync(EndpointUri, updateContent);
+
+        // assert 
+        var response = await Client.GetAsync($"{EndpointUri}/{created.Id}");
+        var placesContent = await response.Content.ReadAsStringAsync();
+        var placesDto = JsonSerializer.Deserialize<PlaceDto>(placesContent, JsonSerializerOptions);
+
+        placesDto.Name.Should().Be("Test1_updated");
+        placesDto.Description.Should().Be($"{nameof(update_changes_place_data)}_1_updated");
+        placesDto.DateCreatedUtc.Should().Be(created.DateCreatedUtc);
+        placesDto.DateModifiedUtc.Should().NotBeNull();
+    }
+    
+    [Test]
+    public async Task update_returns_not_found()
+    {
+        // arrange
+
+        // act
+        var dto = new UpdatePlaceDto(1, "Test1_updated", $"{nameof(update_returns_not_found)}_1_updated");
+        var updateContent = JsonContent.Create(dto);
+        var response = await Client.PutAsync(EndpointUri, updateContent);
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 }
--- HomeMeters2.API/Places/PlaceController.cs
+++ HomeMeters2.API/Places/PlaceController.cs
@@ -18,8 +18,9 @@ public class PlaceController : ControllerBase
         _dbContext = dbContext;
         _logger = logger;
     }
-    
-    [HttpGet()]
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<PlaceDto>), StatusCodes.Status200OK)]
     public async Task<ActionResult<IEnumerable<PlaceDto>>> GetPlaces()
     {
         try
@@ -33,29 +34,9 @@ public class PlaceController : ControllerBase
         }
     }
 
-    [HttpGet("Deleted")]
-    public async Task<ActionResult<IEnumerable<PlaceDeletedDto>>> GetDeletedPlaces()
-    {
-        try
-        {
-            return Ok(await _dbContext.Places.IgnoreQueryFilters().Where(x => x.IsSoftDeleted).Select(x => new PlaceDeletedDto
-            {
-                Id = x.Id,
-                Name = x.Name,
-                Description = x.Description,
-                DateCreatedUtc = x.DateCreatedUtc,
-                DateModifiedUtc = x.DateModifiedUtc,
-                DateSoftDeletedUtc = x.DateSoftDeletedUtc!.Value
-            }).ToListAsync());
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(LogIds.Place.ControllerException, ex, "CreatePlace exception: {Message}", ex.Message);
-            return Problem();
-        }
-    } 
-
     [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(PlaceDeletedDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<PlaceDto>> GetPlace(int id)
     {
         try
@@ -63,7 +44,7 @@ public class PlaceController : ControllerBase
             var place = await _dbContext.Places.FirstOrDefaultAsync(x => x.Id == id);
             if (place is null)
                 return NotFound();
-            
+
             return new PlaceDto
             {
                 Id = place.Id,
@@ -80,19 +61,75 @@ public class PlaceController : ControllerBase
         }
     }
 
+    [HttpGet("Deleted")]
+    [ProducesResponseType(typeof(IEnumerable<PlaceDeletedDto>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<PlaceDeletedDto>>> GetDeletedPlaces()
+    {
+        try
+        {
+            return Ok(await _dbContext.Places.IgnoreQueryFilters().Where(x => x.IsSoftDeleted).Select(x =>
+                new PlaceDeletedDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    DateCreatedUtc = x.DateCreatedUtc,
+                    DateModifiedUtc = x.DateModifiedUtc,
+                    DateSoftDeletedUtc = x.DateSoftDeletedUtc!.Value
+                }).ToListAsync());
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(LogIds.Place.ControllerException, ex, "CreatePlace exception: {Message}", ex.Message);
+            return Problem();
+        }
+    }
+
+    [HttpGet("Deleted/{id:int}")]
+    [ProducesResponseType(typeof(PlaceDeletedDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<PlaceDeletedDto>> GetDeletedPlace(int id)
+    {
+        try
+        {
+            var place = await _dbContext.Places.IgnoreQueryFilters()
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsSoftDeleted);
+            if (place is null)
+                return NotFound();
+
+            return new PlaceDeletedDto
+            {
+                Id = place.Id,
+                Name = place.Name,
+                Description = place.Description,
+                DateCreatedUtc = place.DateCreatedUtc,
+                DateModifiedUtc = place.DateModifiedUtc,
+                DateSoftDeletedUtc = place.DateSoftDeletedUtc ??
+                                     DateTime.SpecifyKind(DateTime.MinValue, DateTimeKind.Utc)
+            };
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(LogIds.Place.ControllerException, ex, "CreatePlace exception: {Message}", ex.Message);
+            return Problem();
+        }
+    }
+
     [HttpPost]
+    [ProducesResponseType(typeof(PlaceDto), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<string>> CreatePlace(CreatePlaceDto dto)
     {
         try
         {
             var place = new Place(dto.Name, dto.Description ?? string.Empty,
                 TimeProvider.System.GetUtcNow().UtcDateTime);
-            
+
             _dbContext.Places.Add(place);
             var count = await _dbContext.SaveChangesAsync();
 
             if (count == 0) return BadRequest("Could not save place.");
-            
+
             return CreatedAtAction(nameof(GetPlace), new { id = place.Id }, new PlaceDto
             {
                 Id = place.Id,
@@ -101,16 +138,18 @@ public class PlaceController : ControllerBase
                 DateCreatedUtc = place.DateCreatedUtc,
                 DateModifiedUtc = place.DateModifiedUtc
             });
-
         }
         catch (Exception ex)
         {
             _logger.LogError(LogIds.Place.ControllerException, ex, "CreatePlace exception: {Message}", ex.Message);
             return Problem();
-        }        
+        }
     }
 
     [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> UpdatePlace(UpdatePlaceDto dto)
     {
         try
@@ -124,8 +163,8 @@ public class PlaceController : ControllerBase
                 return BadRequest("No update took place");
 
             var updated = await _dbContext.SaveChangesAsync();
-        
-            return updated > 0 ? NoContent() : BadRequest("No update took place");   
+
+            return updated > 0 ? NoContent() : BadRequest("No update took place");
         }
         catch (Exception ex)
         {
@@ -135,6 +174,8 @@ public class PlaceController : ControllerBase
     }
 
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult> DeletePlace(int id)
     {
         try
