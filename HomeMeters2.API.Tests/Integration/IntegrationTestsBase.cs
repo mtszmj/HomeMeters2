47794ccd0f9c5c18eac10f001c76f6bc20e77bf7
Mateusz <mateuszmaj@o2.pl>
2023-09-17 20:32:03 +0200

    feat: Update to net8 RC1

--- HomeMeters2.API.Tests/Integration/IntegrationTestsBase.cs
+++ HomeMeters2.API.Tests/Integration/IntegrationTestsBase.cs
@@ -3,8 +3,8 @@ using System.Net.Http.Json;
 using System.Text.Json;
 using HomeMeters2.API.Constants;
 using HomeMeters2.API.DataAccess;
-using Microsoft.AspNetCore.Mvc.Testing;
 using HomeMeters2.API.Extensions;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,74 +14,39 @@ namespace HomeMeters2.API.Tests.Integration;
 [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
 public class IntegrationTestsBase
 {
-    private readonly WebApplicationFactory<Program> _factory;
-    protected readonly HttpClient Client;
+    private WebApplicationFactory<Program> _factory;
+    protected HttpClient Client;
+    private Lazy<HttpClient> LazyLoggedInClient;
     protected HttpClient LoggedInClient => LazyLoggedInClient.Value;
-    private readonly Lazy<HttpClient> LazyLoggedInClient;
 
-    protected JsonSerializerOptions JsonSerializerOptions { get; } = new JsonSerializerOptions
+    protected JsonSerializerOptions JsonSerializerOptions { get; } = new()
     {
         PropertyNameCaseInsensitive = true,
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
     };
 
-    public IntegrationTestsBase()
+    [SetUp]
+    public virtual void SetUp()
     {
         _factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        {
+            builder.FlagAsIntegrationTest();
+            builder.ConfigureServices(services =>
             {
-                builder.FlagAsIntegrationTest();
-                builder.ConfigureServices(services =>
-                {
-                    var descriptor = services.SingleOrDefault(
-                        d => d.ServiceType ==
-                             typeof(DbContextOptions<ApplicationDbContext>));
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<ApplicationDbContext>));
 
-                    if (descriptor != null)
-                    {
-                        services.Remove(descriptor);
-                    }
+                if (descriptor != null) services.Remove(descriptor);
 
-                    var inMemoryDbId = Guid.NewGuid().ToString();
-                    services.AddDbContext<ApplicationDbContext>(options => options
-                        .UseInMemoryDatabase(inMemoryDbId)
-                        .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning)));
-                });
+                var inMemoryDbId = Guid.NewGuid().ToString();
+                services.AddDbContext<ApplicationDbContext>(options => options
+                    .UseInMemoryDatabase(inMemoryDbId)
+                    .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning)));
             });
+        });
         Client = _factory.CreateClient();
         LazyLoggedInClient = new Lazy<HttpClient>(CreateLoggedInClient);
-    }
-
-    private HttpClient CreateLoggedInClient()
-    {
-        var httpClient = _factory.CreateClient();
-        var content = JsonContent.Create(new 
-        {
-            Username = "inttest",
-            Password = "Test1!",
-            Email = "test@test.test1"
-        });
-        var result = httpClient.PostAsync($"{UsersConstants.EndpointPath}/register", content).GetAwaiter().GetResult();
-
-        content = JsonContent.Create(new
-        {
-            Username = "inttest",
-            Password = "Test1!",
-        });
-        var result2 = httpClient.PostAsync($"{UsersConstants.EndpointPath}/login", content).GetAwaiter().GetResult();
-        var loginResultContent = result2.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-        
-        var loginResult = JsonSerializer.Deserialize<LoginResult>(loginResultContent, JsonSerializerOptions);
-
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", loginResult.access_token);
-        return httpClient;
-    }
-    
-    private class LoginResult
-    {
-        public string token_type { get; set; }
-        public string access_token { get; set; }
-        public long expires_in { get; set; }
-        public string refresh_token { get; set; }
     }
 
     [TearDown]
@@ -89,5 +54,40 @@ public class IntegrationTestsBase
     {
         _factory.Dispose();
         Client.Dispose();
+        if (LazyLoggedInClient.IsValueCreated)
+            LazyLoggedInClient.Value.Dispose();
+    }
+
+    private HttpClient CreateLoggedInClient()
+    {
+        var httpClient = _factory.CreateClient();
+        var content = JsonContent.Create(new
+        {
+            Email = "test@test.test1",
+            Password = "Test1!"
+        });
+        var result = httpClient.PostAsync($"{UsersConstants.EndpointPath}/register", content).GetAwaiter().GetResult();
+
+        content = JsonContent.Create(new
+        {
+            Email = "test@test.test1",
+            Password = "Test1!"
+        });
+        var result2 = httpClient.PostAsync($"{UsersConstants.EndpointPath}/login", content).GetAwaiter().GetResult();
+        var loginResultContent = result2.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+        var loginResult = JsonSerializer.Deserialize<LoginResult>(loginResultContent, JsonSerializerOptions);
+
+        httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", loginResult.AccessToken);
+        return httpClient;
+    }
+
+    private class LoginResult
+    {
+        public string TokenType { get; set; }
+        public string AccessToken { get; set; }
+        public long ExpiresIn { get; set; }
+        public string RefreshToken { get; set; }
     }
 }
